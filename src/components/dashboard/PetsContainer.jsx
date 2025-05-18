4d5d99f177ceeda939c67234e48b71146c280a28
Michael Alexis Vasquez Lopera <193014160+MichaelVasquez-Dev@users.noreply.github.com>
2025-05-17 21:05:41 -0500

    first commit

---
+++ src/components/dashboard/PetsContainer.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+import { RegisterPet } from "./RegisterPet";
+import { getAllPetsByUserId } from "../../services/pet.service";
+import { CardPet } from "./CardPet";
+
+export const PetsContainer = () => {
+
+    const [pets, setPets] = useState([]);
+    const [newPetFlag, setNewPetFlag] = useState(false);
+
+    useEffect(()=>{
+        getPets();
+    },[])
+
+    const getPets = async () => {
+        const respPets = await getAllPetsByUserId();
+        console.log(respPets);
+        setPets(respPets);
+        console.log(pets);
+    }
+
+    const back = () => {
+        setNewPetFlag(false);
+        getPets();
+    }
+
+    return (
+        <div className="bg-light text-dark pt-3 ps-5 pe-5 overflow-auto h-100" >
+            {
+                !newPetFlag && <h1>Mis mascotas</h1>
+            }
+            {
+                newPetFlag ? 
+                <RegisterPet back={back} />:
+                <div>
+                    <button className="btn btn-primary" onClick={() => setNewPetFlag(true)}>Agregar mascota</button>
+                    <div className="row mt-3">
+                        {
+                            pets.length > 0 ?
+                            pets.map((pet) => ( <CardPet pet={pet} key={pet.id} />)) :
+                            <h1 className="text-center">No tienes mascotas registradas</h1>
+                        }
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
